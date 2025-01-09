eb223f28f070e279baab3de954eb0b407d4c9142
Santiago <sam2800ml@gmail.com>
2025-01-08 21:46:58 -0500

    adding testing

--- Phase1_Fundametals/QuickStart/QuickStart.py
+++ Phase1_Fundametals/QuickStart/QuickStart.py
@@ -101,3 +101,38 @@ for t in range(epochs):
     train(train_dataloader, model=model, loss_fn=loss_fn, optimizer=optimizer)
     test(test_dataloader, model=model, loss_fn=loss_fn)
 print("Done")
+
+
+# Saving the model 
+torch.save(model.state_dict(), "model.pth")
+print("Model saved as model.pth")
+
+
+# Loading the model 
+
+model = NeuralNetwork().to(device)
+model.load_state_dict(torch.load("model.pth", weights_only=True))
+
+
+# Testing the model behavior
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    x = x.to(device)
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f"Predicted: {predicted}, Actual: {actual}")
